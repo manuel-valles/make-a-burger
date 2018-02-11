0e4a2ee6ec9dc401a918100541c9f99ead402dd4
manukempo <manukempo@gmail.com>
2018-02-11 11:43:37 +0000

    Connected State to Build Controls

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -4,7 +4,7 @@ import styles from './BuildControl.css';
 const buildControl = props => (
     <div className={styles.BuildControl}>
         <div className={styles.Label}>{props.label}</div>
-        <button className={styles.More}>More</button>
+        <button className={styles.More} onClick={props.added}>More</button>
         <button className={styles.Less}>Less</button>
     </div>
 );
