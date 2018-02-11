98f66cfe2ff094eb240982dcab50e746cd36bcfd
manukempo <manukempo@gmail.com>
2018-02-11 10:09:14 +0000

    Message for no ingredients; BuildControl/s components and their CSS

---
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './BuildControl.css';
+
+const buildControl = props => (
+    <div className={styles.BuildControl}>
+        <div className={styles.Label}>{props.label}</div>
+        <button className={styles.More}>More</button>
+        <button className={styles.Less}>Less</button>
+    </div>
+);
+
+export default buildControl;
