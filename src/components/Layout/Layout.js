b00cf28956e16c1bb04fe51bd41254089ffb9147
Manuel Valles <manukempo@gmail.com>
2018-02-12 15:33:57 +0000

    Creating a responsive SideDrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,10 +2,12 @@ import React from 'react';
 import Auxiliar from '../../hoc/Auxiliar';
 import styles from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const layout = (props) => (
     <Auxiliar>
         <Toolbar />
+        <SideDrawer />
         <main className={styles.Content}>
             <p className={styles.Temp}><i className="fa fa-spinner" aria-hidden="true"></i> Under construction</p>
             {props.children}
