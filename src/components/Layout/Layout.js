fd58fd1cb329212fae402663cb6fdd25c46c6c5f
Manuel Valles <manukempo@gmail.com>
2018-02-12 11:42:29 +0000

    Starting Toolbar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Auxiliar from '../../hoc/Auxiliar';
 import styles from './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 const layout = (props) => (
     <Auxiliar>
-        <p className={styles.Temp}><i className="fa fa-spinner" aria-hidden="true"></i> Under construction</p>
-        <div>Toolbar, SideDrawer, Backdrop</div>
+        <Toolbar />
         <main className={styles.Content}>
+            <p className={styles.Temp}><i className="fa fa-spinner" aria-hidden="true"></i> Under construction</p>
             {props.children}
         </main>
     </Auxiliar>
---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './Toolbar.css';
+
+const toolbar = (props) => (
+    <header className={styles.Toolbar}>
+        <div>MENU</div>
+        <div>LOGO</div>
+        <nav>
+            ...
+        </nav>
+    </header>
+);
+
+export default toolbar;
